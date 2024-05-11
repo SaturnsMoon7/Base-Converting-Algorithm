785b05538886a2b7c64d56c367962365f47712e3
SaturnsMoon7 <eduardf@live.wsd1.org>
2024-05-11 15:54:39 -0500

    May11edit

--- Main.java
+++ Main.java
@@ -1,56 +1,64 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
-
 public class Main
-{
-  public static void main(String[] args) 
-  {
-      ArrayList<Integer> numberList = new ArrayList<Integer>();
-      numberList.add(3);
-      numberList.add(4);
-      numberList.add(2);
-      numberList.add(1);
-      System.out.println("Iterative: " + IterativeBaseConverter(numberList, 5));
-      System.out.println("Recursive: " + RecursiveBaseConverter(numberList, 5));
-  }
-  
-  public static int IterativeBaseConverter(ArrayList<Integer> digits, int base)
-  {
-      int num = 0;
-      int numDigits = digits.size();
-
-      for (int i = 0; i < numDigits; i++)
-      {
-          int value = digits.get(i);
-          Double multiplier = Math.pow(base, numDigits - 1 - i); // Flips the position of the digit
-
-          num += value * multiplier; 
-      }
-      return num;
-  }
-
-  public static int RecursiveBaseConverter(ArrayList<Integer> digits, int base)
-  {
-    int numDigits = digits.size();
-    System.out.println("Digit: " + numDigits);
-
-    if (numDigits == 1)
-    { return digits.get(0); }
-
-    else
     {
-      int value = digits.get(0);
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer> numberList = new ArrayList<Integer>();
+        numberList.add(0);
+        numberList.add(2);
+        numberList.add(4);
+        numberList.add(3);
+        int base = 8;
 
-      int multiplier = (int)Math.pow(base, (numDigits-1));
-      System.out.println(base + "^" + (numDigits-1));
-
-      int num = value * multiplier;
-
-      ArrayList<Integer> newList = new ArrayList<Integer>(digits);
-      newList.remove(0);
-
-      return num + RecursiveBaseConverter(newList, base);
+        System.out.println("Iterative: " + IterativeBaseConverter(numberList, base));
+        System.out.println("Recursive: " + RecursiveBaseConverter(numberList, base));
     }
-  }
+
+    public static int IterativeBaseConverter(ArrayList<Integer> digits, int base)
+    {
+        int num = 0;
+        int numDigits = digits.size();
+
+        for (int i = 0; i < numDigits; i++) // Loops through each index of the list
+        {
+            int value = digits.get(i); // The digit in position (i)
+
+            if (value > (base - 1)) // Checks if number is a valid symbol for the base
+            { throw new ArithmeticException("Unknown symbol"); }
+
+            int multiplier = (int)Math.pow(base, (numDigits - 1) - i); // The base to the power of the difference of #digits and (i)
+            num += value * multiplier; 
+        }
+        return num;
+    }
+
+    public static int RecursiveBaseConverter(ArrayList<Integer> digits, int base)
+    {
+        int numDigits = digits.size();
+
+        if (numDigits == 1) // Base case 
+        { return digits.get(0); } // Last digit is raised to the power of 0
+
+        else
+        {
+            int value = digits.get(0); // The digit in position (i)
+            int multiplier = (int)Math.pow(base, (numDigits - 1)); // The base to the power of digits.size()
+            int num = value * multiplier;
+
+            ArrayList<Integer> newList = new ArrayList<Integer>(digits); // Clones digitsList
+            newList.remove(0); // Removes the first element in the list
+
+            return num + RecursiveBaseConverter(newList, base); // Recursion
+        }
+    }
 }
+
+// How to convert double to int
+// How to have multiple data types in a list
+// How to use Math.pow()
+// How to convert base2 --> base10
+// How to convert base10 --> base2
+// How to convert base2 to any base (except base10)
+// How to convert int to string
