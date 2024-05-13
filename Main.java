8ca804cbf1fcb709df806e9a87e2573a2124c66f
SaturnsMoon7 <eduardf@live.wsd1.org>
2024-05-12 22:34:57 -0500

    May 12 Night

--- Main.java
+++ Main.java
@@ -6,14 +6,17 @@ public class Main
     public static void main(String[] args) 
     {
         ArrayList<Integer> numberList = new ArrayList<Integer>();
-        numberList.add(0);
+        numberList.add(7);
         numberList.add(2);
         numberList.add(4);
-        numberList.add(3);
+        numberList.add(3); 
         int base = 8;
 
         System.out.println("Iterative: " + IterativeBaseConverter(numberList, base));
         System.out.println("Recursive: " + RecursiveBaseConverter(numberList, base));
+
+        System.out.println(Log(3, 7));
+        //System.out.println("Iterative: " + IterativeBaseConverterMod(numberList, base, 6));
     }
 
     public static int IterativeBaseConverter(ArrayList<Integer> digits, int base)
@@ -53,7 +56,46 @@ public class Main
             return num + RecursiveBaseConverter(newList, base); // Recursion
         }
     }
-}
+
+    // Argument must be element of Reals
+    public static int Log(int base, int argument)
+    {
+        if ((argument % base) != 0) // Checks if argument is multiple of base (will be false at base if log is not whole #)
+            { throw new ArithmeticException(); }
+
+        if (argument == base)
+        { return 1; }
+        else
+        { argument /= base; }
+
+        return 1 + Log(base, argument);
+    }
+
+    public static int IterativeBaseConverterMod(ArrayList<Integer> digits, int intialBase, int finalBase)
+    {
+        int initialNum = 0;
+        int numDigits = digits.size();
+
+        for (int i = 0; i < numDigits; i++) // Loops through each index of the list
+        {
+            int value = digits.get(i); // The digit in position (i)
+
+            if (value > (intialBase - 1)) // Checks if number is a valid symbol for the base
+            { throw new ArithmeticException("Unknown symbol"); }
+
+            int multiplier = (int)Math.pow(intialBase, (numDigits - 1) - i); // The base to the power of the difference of #digits and (i)
+            initialNum += value * multiplier; 
+        }
+        
+        int finalNum = 0;
+        int remainder = initialNum % finalBase;
+        int digitSize = Log(finalBase, (finalNum - remainder));
+
+        ArrayList<Integer> finalNum
+
+        return finalNum;
+    }
+} 
 
 // How to convert double to int
 // How to have multiple data types in a list
@@ -62,3 +104,4 @@ public class Main
 // How to convert base10 --> base2
 // How to convert base2 to any base (except base10)
 // How to convert int to string
+// Do {} While {}
